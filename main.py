84de9c7fe4f69c60f21357e7a24a263cf755ef56
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-06-29 20:27:13 +0300

    Alun valintanapit luotu

--- main.py
+++ main.py
@@ -14,32 +14,50 @@ from random import randrange
 from tkinter import *
 
 
-class Game_GUI:
+class GameGUI:
     def __init__(self):
+        # Attributes
         self.__amount_of_mistakes = 0
         self.__correct_answer = ""
-        self.__correct_words = ["hevonen", "kaupunki", "Suomi"]
+        self.__correct_words = ["xyzz:-)", "kaupunki:D", "SuomiÖ", "moi:)"]
         self.__has_word_been_guessed = False
-        self.__mainwindow = Tk()
-        self.__random_index_value = 0
+        self.__is_game_type_multiplayer = False
         self.__list_length = len(self.__correct_words)
+        self.__main_window = Tk()
+        self.__random_index_value = 0
 
-    def start(self):
-        """
-        Starts the mainloop.
-        """
-        self.__mainwindow.mainloop()
 
+        if self.choose_game_type():
+            self.generate_random_index_value()
+
+        else:
+            self.input_word_to_be_guessed()
+
+        self.generate_game_board()
+        self.__main_window.mainloop()
+
+    def choose_game_type(self):
+
+        self.__singleplayer = Button(self.__main_window, text="Singleplayer")
+        self.__singleplayer.grid(row=0, column=0)
+
+        self.__multiplayer = Button(self.__main_window, text="Multiplayer")
+        self.__multiplayer.grid(row=0, column=1)
 
     def generate_random_index_value(self):
-        return randrange(self.__list_length)
+        self.__random_index_value = randrange(self.__list_length)
 
+    def input_word_to_be_guessed(self):
+        pass
 
-
+    def generate_game_board(self):
+        pass
 
 
 def main():
-    ui = Game_GUI()
-    ui.start()
+    ui = GameGUI()
+
+
 if __name__ == "__main__":
     main()
+
