b632d84d6827d5aa3c015caac03a049a69af0f37
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-07-16 17:24:13 +0300

    Napit menee listalle

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ class GameGUI:
         self.__amount_of_mistakes = 0
         self.__correct_answer = ""
         self.__correct_words = ["koiruli", "kisse", "rät", "hevone"]
+        self.__keyboard = []
         self.__game_title = ""
         self.__has_word_been_guessed = False
         self.__is_game_type_multiplayer = False
+        self.__was_inputted_word_legal = False
         self.__list_length = len(self.__correct_words)
         self.__main_window = Tk()
         self.__random_index_value = 0
@@ -31,7 +33,14 @@ class GameGUI:
         self.choose_game_type()
 
         self.generate_game_board()
+
+        if self.__was_inputted_word_legal:
+            print("Arvo oli laillinen.")
+
+
         self.__main_window.mainloop()
+
+
 
     def choose_game_type(self):
 
@@ -45,6 +54,9 @@ class GameGUI:
         self.__multiplayer = Button(self.__main_window, text="Multiplayer",
                                     command=self.input_word_to_be_guessed)
         self.__multiplayer.grid(row=1, column=2)
+
+    def print_a_letter_by_clicking_a_button(self):
+        pass
 
     def generate_random_index_value(self):
         # self.__is_game_type_multiplayer = False
@@ -62,9 +74,15 @@ class GameGUI:
 
         if not self.is_alpha():
             self.__enter_word.configure(bg='red')
+            self.__was_inputted_word_legal = False
 
         else:
             self.__enter_word.configure(bg='white')
+            self.__was_inputted_word_legal = True
+
+            if self.__was_inputted_word_legal:
+                print("Arvo oli laillinen.")
+                return
 
 
 
@@ -81,6 +99,7 @@ class GameGUI:
 
 
 
+
     def input_word_to_be_guessed(self):
         # self.__is_game_type_multiplayer = True
         self.__enter_word = Entry()
@@ -89,6 +108,8 @@ class GameGUI:
         self.__choose_button = Button(self.__main_window, text="Choose",
                                       command=self.check_if_word_is_legal)
         self.__choose_button.grid(row=2, column=3)
+        # if self.__was_inputted_word_legal:
+        #     print("Arvo oli laillinen.")
 
 
 
@@ -112,13 +133,14 @@ class GameGUI:
 
     def generate_game_board(self):
 
-        keyboard = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "å"],
+        self.__keyboard = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+                          "å"],
                     ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä"],
                     ["z", "x", "c", "v", "b", "n", "m"]]
 
         current_row = 3
         current_column = 3
-        for row in keyboard:
+        for row in self.__keyboard:
             if current_row == 5:
                 current_column = 5
 
@@ -146,4 +168,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
